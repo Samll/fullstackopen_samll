1f38d88f586b3447f78193a74eeff0513a870e5c
Tote <totepubli@gmail.com>
2024-07-11 19:44:22 +0200

    Update learning until 3.13

--- part3/learning/index.js
+++ part3/learning/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Note = require('./models/note')
 const cors = require('cors')
 app.use(express.static("dist"))
-app.use(cors())
-
+app.use(cors())  
 
 
 const requestLogger = (request, response, next) => {
@@ -17,32 +18,15 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 app.use(requestLogger)
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hola a todo el mundo!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
   
-  console.log("Retrieving all notes")
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 const generateId = () => {
@@ -55,70 +39,47 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false
+  })
+
+  note.save().then(savedNote =>{
+    response.json(savedNote)
+  })
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
   const note = {
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
+    important: body.important,
   }
 
-  notes = notes.concat(note)
-
-  response.json(note)
-})
-
-app.put('/api/notes/:id', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
     })
-  }
-
-  const id = Number(request.params.id) 
-  const n = notes.filter(note => note.id === id)
-  if (!n){
-    return response.status(400).json({
-      error: 'note ID not existing in database'
-    })
-  }
-  console.log(n.id)
-  const newNote = {
-    content: body.content,
-    important: Boolean(body.important) || false,
-    id: id,
-  }
-  notes = notes.filter(note => note.id !== id).concat(newNote)
-
-  response.json(newNote)
+    .catch(error => next(error))
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id) 
-  const note = notes.find(note => note.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note =>{
     response.json(note)
-  }else{
-    response.statusMessage = "<h1>The requested ID is not existing for Notes</h1>";
-    response.status(400).end()
-  } 
+  }) 
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id) 
-  notes = notes.filter(note => note.id !== id)
-  if (note) {
-    response.status(204).end()
-  }else{
-    response.statusMessage = "<h1>The requested ID is not existing for Notes</h1>";
-    response.status(400).end()
-  } 
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -126,7 +87,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- part3/learning/mongo.js
+++ part3/learning/mongo.js
@@ -20,6 +20,15 @@ const noteSchema = new mongoose.Schema({
 })
 
 const Note = mongoose.model('Note', noteSchema)
+
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+  
 /* 
 const note = new Note({
   content: 'HTML is easy',
