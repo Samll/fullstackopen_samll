3c05a6a7f426b26deabf191095ec1fd6b69aa0af
Tote <totepubli@gmail.com>
2024-07-12 10:21:43 +0200

    Add Lint on Lint file

--- part3/phonebook_mongodb/backend/eslint.config.mjs
+++ part3/phonebook_mongodb/backend/eslint.config.mjs
@@ -1,40 +1,40 @@
-import stylisticJS from "@stylistic/eslint-plugin-js";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import stylisticJS from '@stylistic/eslint-plugin-js'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
 
 export default [
-    ...compat.extends("eslint:recommended"),
-    { 
-        languageOptions: {
-            globals: { node: true },
-        },
-        plugins: {
-            "@stylistic/js": stylisticJS,
-        },
-        rules: {
-            "@stylistic/js/indent": ["error", 2],
-            "@stylistic/js/linebreak-style": ["error", "unix"],
-            "@stylistic/js/quotes": ["error", "single"],
-            "@stylistic/js/semi": ["error", "never"],
-            'eqeqeq' : 'error',
-            'no-trailing-spaces': 'error',
-            'object-curly-spacing': [
-                'error', 'always'
-            ],
-            'arrow-spacing': [
-                'error', { 'before': true, 'after': true }
-            ],
-            'no-console': 0
-        },
+  ...compat.extends('eslint:recommended'),
+  {
+    languageOptions: {
+      globals: { node: true },
     },
-];
+    plugins: {
+      '@stylistic/js': stylisticJS,
+    },
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+      'eqeqeq' : 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true }
+      ],
+      'no-console': 0
+    },
+  },
+]
