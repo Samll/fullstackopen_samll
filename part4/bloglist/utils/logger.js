8fe643a1be71f3a491fb5312be55b2447866e1e2
Tote <totepubli@gmail.com>
2024-12-04 00:19:49 +0100

    Finally on test logger will not print
    I forgot I modified it to print always as I was finding errors

--- part4/bloglist/utils/logger.js
+++ part4/bloglist/utils/logger.js
@@ -1,11 +1,11 @@
 const info = (...params) => {
-  if (process.env.NODE_ENV !== 'testm') { 
+  if (process.env.NODE_ENV !== 'test') { 
     console.log(...params)
   }
   }
   
   const error = (...params) => {
-    if (process.env.NODE_ENV !== 'testm') { 
+    if (process.env.NODE_ENV !== 'test') { 
       console.error(...params)
     }
   }
